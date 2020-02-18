569693aa8a07af1455a8a0db200a592a5aa5e71f
zach-e-the-robot <benjaminmwesley@gmail.com>
2020-02-17 20:50:05 -0500

    began implementation of parser

---
+++ c/parser.js
@@ -0,0 +1,19 @@
+pl.c.parseCommand = function (command) {
+    args = command.split(" ");
+
+    switch (args[0]) {
+        case "add":    
+        var slots = {
+                name: args[1]
+            };
+            UMLClass.add(slots);
+            break;
+        case "delete":
+            UMLClass.destroy(args[1]);
+            break;
+        case "export":
+            UMLClass.exportFile();
+            break;
+
+    }
+}
