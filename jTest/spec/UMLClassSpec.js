94cc7cd788f6ab906645510e0bb7b5c60c7e4a19
qnjones <60272002+qnjones@users.noreply.github.com>
2020-03-13 11:55:29 -0400

    not testing console log anymore, just functions

--- jTest/spec/UMLClassSpec.js
+++ jTest/spec/UMLClassSpec.js
@@ -1,7 +1,21 @@
 // TEST FILE FOR UML CLASS SOURCE FILE
+/*
+Using describe() Blocks:
+Jasmine uses a built-in describe block (it's actually a JavaScript function) to 
+separate entire sets of functionality. The first parameter to the describe() is 
+a name for the test "suite", and tells Jasmine (and yourself/other programmers) 
+that everything inside this describe block is dealing with that specific part of 
+your application.
 
+Using it() Blocks:
+Another built-in Jasmine block (function) is the it() block. This block represents 
+a single unit test. You can have multiple it() blocks inside of one describe() block, 
+which represent multiple unit tests inside of a "test suite." The first parameter to 
+the it() block is an explanation of what the function you're testing should do. 
+VERY commonly, it reads like a sentence beginning with the word "should"
+*/
 // TESTS FOR ADD CASES:
-describe("should test for UMLclass.add console ouput", function () {
+describe("should test two cases for UMLclass.add", function () {
     //beforeEach(function() {
     	//spyOn(console, 'log');
     //});
@@ -18,7 +32,7 @@ describe("should test for UMLclass.add console ouput", function () {
 });
 
 //TESTS FOR DELETE CASES:
-describe("should test for UMLclass.destroy console ouput", function () {
+describe("should test two cases for UMLclass.destroy", function () {
     //beforeEach(function() {
     	//spyOn(console, 'log');
     //});
