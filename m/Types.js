58085b9998ed5aa884da5c4b142bf092a2ecbdd8
qnjones <60272002+qnjones@users.noreply.github.com>
2020-03-26 13:46:44 -0400

    Types.js for the Relationship types enumerator

---
+++ m/Types.js
@@ -0,0 +1,10 @@
+/* 
+Decided to put the relationship types we will be defining into an enumerator so that 
+we can use them as identify edge types 
+*/
+
+const TypeRel = {
+	INHERITANCE: 'inheritance',
+	COMPOSITION: 'composition',
+	AGGREGATION: 'aggregation',
+}
