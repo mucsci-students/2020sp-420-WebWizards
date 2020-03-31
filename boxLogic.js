204b211a8729c07430435e7cc4ddb69ab12db3ae
kendall1812 <60229373+kendall1812@users.noreply.github.com>
2020-03-31 12:49:48 -0400

    Box is draggable and then disappears

--- boxLogic.js
+++ boxLogic.js
@@ -1,44 +1,28 @@
+function onDragStart(event) {
+  event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
 
-        //Make the DIV element draggagle:
-dragElement(document.getElementById("classBox"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "classBox")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "classBox").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
+  event
+    .currentTarget
+    .style
   }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+function onDragOver(event) {
+    event.preventDefault();
   }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function onDrop(event) {
+  const id = event
+    .dataTransfer
+    .getData('text');
+  
+  const draggableElement = document.getElementById(id);
+  const dropzone = event.target;
+    
+  dropzone.appendChild(document.getElementById(id));
+  
+  event
+    .dataTransfer
+    .clearData();
 }
