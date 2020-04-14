ed5f133e9de9421c9e25ca60aa5a3d7084d765b3
JusJacs <47124849+JusJacs@users.noreply.github.com>
2020-04-14 14:29:26 -0400

    can add class to dorp zone, ned to work on a class constructor

--- boxLogic.js
+++ boxLogic.js
@@ -1,45 +1,17 @@
-const classBox = document.querySelector('.classBox');
-const dropArea = document.querySelectorAll('.dropArea');  //puts in loop since multiple node list
+ function allowDrop(event) {
+        event.preventDefault();
+        console.log("allowDrop fired");
+    }
 
-//Listeners for fill (our box) if to fix uncaught null
-classBox.addEventListener('dragstart', dragStart);
-classBox.addEventListener('dragend', dragEnd);
-classBox.addEventListener('dragover', dragOver);
+    function drag(event) {
+        event.dataTransfer.setData("text", event.target.id);
+        console.log("drag fired");
+    }
 
-
-// loop through all draggable divs
- var dm = document.getElementsByClassName('classBox');
- for (var i = 0; i < dm.length; i++) {
-  dm[i].addEventListener('dragstart', drag_start, false);
-  document.body.addEventListener('dragover', drag_over, false);
-  document.body.addEventListener('drop', drop, false);
-}
-
-//Drag Functions
-
-function dragStart(){
-  console.log('drag start');
-  var style = window.getComputedStyle(event.target, null);
-  event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
-  //setTimeout(() => , 0 )    for lag issue
-
-
-}
-
-function dragEnd() {
-  console.log('drag end');
-  var offset = event.dataTransfer.getData("text/plain").split(',');
-  var dm = document.getElementsByClassName('classBox');
-  dm[parseInt(offset[2])].style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-  dm[parseInt(offset[2])].style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
-  event.preventDefault();
-
-}
-
-function dragOver(e) {
-  e.preventDefault()
-  console.log('drag over');
-
-}
-
+    function drop(event) {
+        event.preventDefault();
+        var data = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(data));
+        console.log("dropped fired");
+    }
 
